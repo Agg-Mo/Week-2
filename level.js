5b260b05c7f7857a613afe1502919b0503138463
Agg-Mo <Jake.R.Ellis@hotmail.com>
2015-09-28 16:34:16 +1000

    Initial Commit
    +added input
    +added map
    +added drawing the map

---
+++ level.js
@@ -0,0 +1,105 @@
+var level = { "height":15,
+ "layers":[
+        {
+         "data":[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 170, 0, 0, 0, 0, 0, 187, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 74, 0, 44, 0, 44, 0, 75, 40, 0, 0, 0, 0, 0, 128, 128, 0, 128, 0, 133, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 142, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 142, 23, 142, 23, 132, 40, 40, 40, 0, 40, 0, 41, 0, 0, 0, 0, 0, 0, 0, 142, 40, 0, 0, 0, 0, 0, 0, 0, 0, 40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 142, 142, 40, 142, 40, 142, 40, 178, 40, 0, 40, 0, 40, 0, 41, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 40, 0, 0, 0, 40, 0, 0, 0, 0, 0, 0, 0, 44, 0, 44, 0, 44, 0, 44, 0, 44, 0, 59, 0, 40, 0, 40, 0, 40, 40, 40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 41, 0, 23, 0, 107, 0, 107, 0, 107, 0, 107, 0, 107, 40, 167, 40, 167, 0, 167, 0, 40, 40, 40, 0],
+         "height":15,
+         "name":"Tile Layer 1",
+         "opacity":1,
+         "type":"tilelayer",
+         "visible":true,
+         "width":20,
+         "x":0,
+         "y":0
+        }, 
+        {
+         "data":[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 151, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 153, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 40, 0, 137, 0, 40, 0, 153, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 149, 0, 0, 0, 148, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 40, 0, 136, 0, 40, 0, 153, 41, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 154, 0, 154, 0, 154, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         "height":15,
+         "name":"Tile Layer 2",
+         "opacity":1,
+         "type":"tilelayer",
+         "visible":true,
+         "width":20,
+         "x":0,
+         "y":0
+        }],
+ "nextobjectid":1,
+ "orientation":"orthogonal",
+ "properties":
+    {
+
+    },
+ "renderorder":"right-down",
+ "tileheight":35,
+ "tilesets":[
+        {
+         "firstgid":1,
+         "image":"tileset.png",
+         "imageheight":1024,
+         "imagewidth":1024,
+         "margin":2,
+         "name":"tileset",
+         "properties":
+            {
+
+            },
+         "spacing":2,
+         "tilecount":196,
+         "tileheight":70,
+         "tilewidth":70
+        }],
+ "tilewidth":35,
+ "version":1,
+ "width":20
+};
+
+var LAYER_COUNT = level.layers.length;
+
+var MAP = 
+	{
+		tw: level.width,
+		th: level.height
+	};
+	
+var TILE = 35;
+
+var TILESET_TILE = TILE * 2;
+var TILESET_PADDING = 2;
+var TILESET_MARGIN = 2;
+
+var TILESET_COUNT_X = 14;
+var TILESET_COUNT_Y = 14;
+
+var tileset = document.createElement("img");
+tileset.src = "tileset.png";
+
+function drawMap()
+{
+	for(var layerIdx = 0; layerIdx < LAYER_COUNT; layerIdx++)
+	{
+		var idx = 0;
+		for (var y = 0; y < level.layers[layerIdx].height; y++)
+		{
+			for (var x = 0; x < level.layers[layerIdx].width; x++)
+			{
+				if (level.layers[layerIdx].data[idx] != 0)
+				{
+					var tileIndex = level.layers[layerIdx].data[idx] - 1;
+					
+					var sx = TILESET_PADDING + (tileIndex % TILESET_COUNT_X) 			   * (TILESET_TILE + TILESET_PADDING);
+					var sy = TILESET_PADDING + (Math.floor( tileIndex / TILESET_COUNT_Y )) * (TILESET_TILE + TILESET_PADDING);
+					
+					context.drawImage(tileset, sx, sy, 				TILESET_TILE, TILESET_TILE, 
+										x*(TILE), (y - 1) * (TILE), TILESET_TILE, TILESET_TILE);
+					
+				}
+			idx++
+			}
+		}
+	}
+	
+	
+}
+
+
+
+
---
+++ player.js
@@ -0,0 +1,32 @@
+var player = function ()
+{
+	this.image = document.createElement("img");
+	this.x = canvas.width / 2;
+	this.y = canvas.height / 2;
+	this.width = 159;
+	this.height = 163;
+	this.rotation = 0;
+	
+	this.image.src = "hero.png";
+} 
+
+player.prototype.update = function(deltaTime)
+{
+	if (keyboard.isKeyDown(keyboard.KEY_SPACE))
+	{	
+		player.rotation += deltaTime
+	}
+	else
+	{
+		player.rotation -= deltaTime
+	}
+}
+
+player.prototype.draw = function()
+{
+	context.save();
+		context.translate(this.x, this.y);
+		context.rotate(this.rotation);
+		context.drawImage(this.image, -this.width/2, -this.height/2);
+	context.restore();
+}
